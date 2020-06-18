db27de82feab85311972661334b7eeaecdfe9df7
Devdevloper <deveshtv252000@gmail.com>
2020-06-18 09:49:26 +0530

    size

--- form.js
+++ form.js
@@ -1,3 +1,19 @@
+{
+    var firebaseConfig = {
+        apiKey: "AIzaSyCf9ykEI2UyZZSy6Dbs3tulwbSlDkyryfs",
+        authDomain: "fir-6fe10.firebaseapp.com",
+        databaseURL: "https://fir-6fe10.firebaseio.com",
+        projectId: "fir-6fe10",
+        storageBucket: "fir-6fe10.appspot.com",
+        messagingSenderId: "80895933927",
+        appId: "1:80895933927:web:fe63e600e9f0c8fcaad142",
+        measurementId: "G-ZTZ2498BHZ"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+}
+
 function log() {
     const auth = firebase.auth();
     var u = document.getElementById("user");
@@ -8,50 +24,32 @@ function log() {
     } else {
         //const auth = firebase.auth();
         const promise = auth.signInWithEmailAndPassword(u.value, p.value).then(function(result) {
-			
-	
-var nuser = firebase.auth().currentUser;
 
-if (nuser != null) {
-	
-	var namehere="";
-	
-	var ref = firebase.database().ref("Private/Users/" + nuser.uid+"/UserData");
-ref.once("value")
-  .then(function(snapshot) {
-    namehere = snapshot.child("Name").val();
-				            alert("Login Success\nHi "+namehere);
-							
-							
-							
-							
-							
+            var nuser = firebase.auth().currentUser;
 
-  });
-	
-			
-			
-						//window.open("sign.html", "_self");
-}
-			
-			
-						
+            if (nuser != null) {
 
-			
-		}).catch(function(error) {
-    // Handle error.
-	alert(error.message);
-  })
-  
-  
-  
+                var namehere = "";
+
+                var ref = firebase.database().ref("Private/Users/" + nuser.uid + "/UserData");
+                ref.once("value")
+                    .then(function(snapshot) {
+                        namehere = snapshot.child("Name").val();
+                        alert("Login Success\nHi " + namehere);
+                    });
+                //window.open("sign.html", "_self");
+            }
+
+        }).catch(function(error) {
+            // Handle error.
+            alert(error.message);
+        })
         promise.catch(e => alert(e.message));
-
 
         // Handle Errors here.
         //window.alert("Warning : Enter proper details");
     }
-  
+
 
 }
 
