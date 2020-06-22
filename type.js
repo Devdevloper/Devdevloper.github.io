2a3dd61dd82e7cf7f816522ee45c5906e3720b93
Devdevloper <deveshtv252000@gmail.com>
2020-06-22 14:53:15 +0530

    Update type.js

--- type.js
+++ type.js
@@ -2,16 +2,28 @@ function register() {
     const x = document.getElementById("Log-in");
     const y = document.getElementById("register");
     const z = document.getElementById("btn");
-    x.style.left = "-400px";
-    y.style.left = "25px";
-    z.style.left = "110px";
+    if (screen.width > 900) {
+        x.style.left = "-400px";
+        y.style.left = "50px";
+        z.style.left = "110px";
+    } else {
+        x.style.left = "-400px";
+        y.style.left = "25px";
+        z.style.left = "110px";
+    }
 }
 
 function login() {
     const x = document.getElementById("Log-in");
     const y = document.getElementById("register");
     const z = document.getElementById("btn");
-    x.style.left = "25px";
-    y.style.left = "425px";
-    z.style.left = "0px";
+    if (screen.width > 900) {
+        x.style.left = "50px";
+        y.style.left = "450px";
+        z.style.left = "0px";
+    } else {
+        x.style.left = "25px";
+        y.style.left = "425px";
+        z.style.left = "0px";
+    }
 }
