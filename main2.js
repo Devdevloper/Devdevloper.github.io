fc531acbe6a3365678b1f04122b6959a3c158ba8
Devdevloper <deveshtv252000@gmail.com>
2020-06-25 10:48:44 +0530

    Newl

--- main2.js
+++ main2.js
@@ -2,6 +2,6 @@ function register() {
     window.open("registeration.html", "_self");
 }
 
-function start() {
-    window.open("main2.html", "_self");
+function course() {
+    window.alert("No course available");
 }
